aee6a64ee030ce1d740395fa20ebcaf2734fdbeb
Yaroslava Yemets <77026107+yaroslava-yemets@users.noreply.github.com>
2021-12-13 12:02:38 +0200

    удалила

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.module.css";
 
 import Header from "./components/Header";
-import ContainerTabs from "./components/ContainerTabs";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ContainerTabs />
     </div>
   );
 }
