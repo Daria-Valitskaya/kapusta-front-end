89fa13acaa6471274bdd341663d230b9d7833122
Sergey-Markov <1nativefl@gmail.com>
2021-12-13 11:46:14 +0200

    добавил возможность переопредилений стилей через проп класснейм

--- src/Components/Buttons/StandartBtn/StandartBtn.js
+++ src/Components/Buttons/StandartBtn/StandartBtn.js
@@ -2,9 +2,9 @@ import { CommunButton } from "..";
 import s from "./standartBtn.module.css";
 
 //передаем через пропсы текст кнопки
-export default function StandartBtn({ children }) {
+export default function StandartBtn({ children, className }) {
   return (
-    <CommunButton type="button" className={s.btn}>
+    <CommunButton type="button" className={s.btn + " " + className}>
       {children}
     </CommunButton>
   );
