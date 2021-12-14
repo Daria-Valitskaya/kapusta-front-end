607dbac4f6ab9ad0180541be03c85178af647f62
Sergey-Markov <1nativefl@gmail.com>
2021-12-14 17:45:36 +0200

    добавил предварительные роуты /login, /registration, /homeview

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 import "./App.module.css";
 
+import { lazy, Suspense, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import Registration from "./Components/Registration/Registration.js";
+import PrivateRoute from "./Components/PrivatRoute/PrivatRoute";
+import Login from "./Components/Login/Login.js";
+// import { useDispatch, useSelector } from "react-redux";
+import authSelectors from "./redux/auth/auth-selectors";
+import authOperations from "./redux/auth/auth-operations";
+
 import Container from "./Components/Container";
 import Header from "./Components/Header";
-import ContainerTabs from "./Components/ContainerTabs/ContainerTabs";
+// import ContainerTabs from "./Components/ContainerTabs/ContainerTabs";
+import PublicRoute from "./Components/PublicRoute/PublicRoute";
+import HomeViews from "./views/HomeViews/HomeViews";
 
 function App() {
   return (
+    <div>
+      <Container>
+        <Suspense fallback={<h1>LOADING...</h1>}>
+          <Header />
+          <Switch>
+            {/* <PublicRoute component={WellcomePage} exact path="/" /> */}
+            <PublicRoute
+              component={Login}
+              exact
+              path="/login"
+              restricted
+              redirectTo="/homeview"
+            />
+            <PublicRoute
+              component={Registration}
+              exact
+              path="/registration"
+              restricted
+              redirectTo="/login"
+            />
+            <PrivateRoute
+              component={HomeViews}
+              exact
+              path="/homeview"
+              redirectTo="/login"
+            />
 
-    <Container>
-      <div className="App">
-        <Header />
-      <ContainerTabs />
+            {/* <Route>
+              <NotFoundView />
+            </Route> */}
+          </Switch>
+        </Suspense>
+      </Container>
     </div>
-    </Container>
-
   );
 }
 
