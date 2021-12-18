e81c8b4574967a2df68161a119262694edfeec0e
Sergey-Markov <1nativefl@gmail.com>
2021-12-18 19:29:29 +0200

    создал компонент ReportView

---
+++ src/views/ReportView/ReportView.js
@@ -0,0 +1,10 @@
+import IncomeExpenses from "../../Сomponents/IncomeExpenses/IncomeExpenses";
+
+export default function ReportView() {
+  return (
+    <div>
+      <IncomeExpenses />
+      <p>ReportView</p>
+    </div>
+  );
+}
