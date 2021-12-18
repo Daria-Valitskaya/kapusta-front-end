78eab8b4752d8b2c51b5269f0d8da532d1d55af7
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-12-19 00:13:53 +0200

    login, signup errors handle added to redux + some new state fields

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,18 +1,17 @@
-const getIsLoggedIn = (state) => state.auth.isLoggedIn;
 const getUserName = (state) => state.auth.user.name;
 const getIsFetchingCurrent = (state) => state.auth.isFetchingCurrent;
-const getErrorMessage = (state) => state.auth.errorMessage;
-const getIsVerificationEmailSent = (state) =>
-  state.auth.isVerificationEmailSent;
+const getIsRegistered = (state) => state.auth.isRegistered;
 const getIsVerified = (state) => state.auth.isVerified;
+const getIsLoggedIn = (state) => state.auth.isLoggedIn;
+const getErrorMessage = (state) => state.auth.errorMessage;
 
 const authSelectors = {
-  getIsLoggedIn,
   getUserName,
   getIsFetchingCurrent,
-  getErrorMessage,
-  getIsVerificationEmailSent,
+  getIsRegistered,
   getIsVerified,
+  getIsLoggedIn,
+  getErrorMessage,
 };
 
 export default authSelectors;
--- src/Сomponents/PrivatRoute/PrivatRoute.js
+++ src/Сomponents/PrivatRoute/PrivatRoute.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import authSelectors from "../../redux/auth/auth-selectors";
 import { useSelector } from "react-redux";
+import { Redirect, Route } from "react-router-dom";
+import authSelectors from "../../redux/auth/auth-selectors";
 
 const PrivateRoute = ({ redirectTo, children, ...routeProps }) => {
   // Комент на время тестов, чтоб не писпть постоянно логин
-  // const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
   // Удалить перед продакшн
-  const isLoggedIn = true;
+  // const isLoggedIn = true;
   return (
     <Route {...routeProps}>
       {isLoggedIn ? children : <Redirect to={redirectTo} />}
