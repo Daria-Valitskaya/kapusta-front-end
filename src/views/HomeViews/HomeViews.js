6e3752c05d706af357439a0e71a5cd3160f00882
Daria-Valitskaya <75114227+Daria-Valitskaya@users.noreply.github.com>
2021-12-21 01:57:07 +0200

    инпут

--- src/views/HomeViews/HomeViews.js
+++ src/views/HomeViews/HomeViews.js
@@ -1,11 +1,13 @@
 import BalansForm from "../../Сomponents/BalansForm/BalansForm";
 import ContainerTabs from "../../Сomponents/ContainerTabs";
+import InputPanel from "../../Сomponents/InputPanel/InputPanel";
 import MobileTabs from "../../Сomponents/MobileTabs";
 
 export default function HomeViews() {
   return (
-    <div style={{paddingBottom: '57px'}}>
+    <div style={{ paddingBottom: "57px" }}>
       <BalansForm />
+      <InputPanel />
       <MobileTabs />
       <ContainerTabs />
     </div>
