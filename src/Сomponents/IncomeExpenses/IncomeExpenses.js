72320186f80e10b0d23dd82cd116c0536055f15c
Daria Valitskaya <75114227+Daria-Valitskaya@users.noreply.github.com>
2021-12-22 19:00:57 +0200

    Revert "Last fix"

--- src/Сomponents/IncomeExpenses/IncomeExpenses.js
+++ src/Сomponents/IncomeExpenses/IncomeExpenses.js
@@ -1,33 +1,18 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
-import transactionsSelectors from "../../redux/transactions/transactions-selectors";
-
 import s from "./incomeExpenses.module.css";
 
-export default function IncomeExpenses() {
-  const income = useSelector(transactionsSelectors.getAllIncome);
-  const expenses = useSelector(transactionsSelectors.getAllExpenses);
-
-  const allIncome = income.reduce(function (accumulator, { sum }) {
-    return (accumulator += parseFloat(sum));
-  }, 0);
-  const allExpenses = expenses.reduce(function (accumulator, { sum }) {
-    return (accumulator += parseFloat(sum));
-  }, 0);
-
+export default function IncomeExpenses({
+  income = "+45000.00",
+  expenses = "-18000.00",
+}) {
   return (
     <div className={s.field}>
       <div className={s.expenses_field}>
         <p className={s.title}>Расходы:</p>
-        <p className={s.expenses}>
-          {allExpenses ? "-" + allExpenses : "00.00"} грн.
-        </p>
+        <p className={s.expenses}>{expenses} грн.</p>
       </div>
       <div className={s.income_field}>
         <p className={s.title}>Доходы:</p>
-        <p className={s.income}>
-          {allIncome > 0 ? "+" + allIncome : "00.00"} грн.
-        </p>
+        <p className={s.income}>{income} грн.</p>
       </div>
     </div>
   );
